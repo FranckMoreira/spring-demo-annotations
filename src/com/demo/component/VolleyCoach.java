043d5ec5c98e702b7241d8849ecd9fa4524d8ec2
Franck <franck.c.moreira@gmail.com>
2020-07-18 15:53:36 -0300

    Usando setter injection com @Autowired

--- src/com/demo/component/VolleyCoach.java
+++ src/com/demo/component/VolleyCoach.java
@@ -1,9 +1,18 @@
 package com.demo.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.demo.service.FortuneService;
 
 @Component
 public class VolleyCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public VolleyCoach() {
+		System.out.println(">> VolleyCoach: inside default constructor");
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -12,7 +21,13 @@ public class VolleyCoach implements Coach {
 	
 	@Override
 	public String getDailyFortune() {
-		return null;
+		return fortuneService.getFortune();
+	}
+	
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">> VolleyCoach: inside setFortuneService");
+		this.fortuneService = fortuneService;
 	}
 
 }
